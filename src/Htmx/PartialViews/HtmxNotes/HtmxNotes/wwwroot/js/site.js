8ae0b5633c324c08bab48edea78c2968a312a1e0
Richard <rahayn@yahoo.com>
2023-09-27 15:05:42 -0400

    updating gitignore

---
+++ src/Htmx/PartialViews/HtmxNotes/HtmxNotes/wwwroot/js/site.js
@@ -0,0 +1,36 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+document.body.addEventListener("htmx:confirm", (event) => {
+
+    if (event.detail.path === '/Notes?handler=Clear') {
+        event.preventDefault();
+        swal({
+            title: "",
+            text: "Are you sure you want to delete all?",
+            icon: "warning",
+            button: "Proceed"
+        }).then((confirmed) => {
+            if (confirmed) {
+                event.detail.issueRequest();
+            }
+        })
+    }
+
+});
+
+document.body.addEventListener("htmx:beforeSwap", (event) => {
+    if (event.detail.xhr.status !== 200) {
+        swal({
+            title: "Error?",
+            text: "An error occurred in submitting the request!",
+            icon: "warning",
+            dangerMode: true,
+        })
+    }
+    else {
+        swal("Done!","Request was successful","success");
+    }
+});
